9b74909f295d58c216fcb736bbc3ebb72634f7e0
Ubaidmwd <ubaidmwd@gmail.com>
2024-04-09 13:09:46 +0500

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from "react-query/devtools";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
@@ -11,8 +13,13 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient} contextSharing={true}>
       <ChakraProvider>
-        <Home />
+        <Router>
+          <Routes>
+            <Route path="/:id" element={<Home />} />
+          </Routes>
+        </Router>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,101 @@
-// Home.jsx
-
 import React from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
-import { Heading } from "@chakra-ui/react";
+import {
+  Heading,
+  Spinner,
+  Container,
+  Stack,
+  Flex,
+  Text,
+  Grid,
+  useToast,
+  Button,
+} from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
 
-const fetchData = async () => {
+const fetchData = async (pageId) => {
   try {
-    const { data } = await axios.get("https://gorest.co.in/public/v2/users");
+    const { data } = await axios.get(
+      `https://gorest.co.in/public/v1/posts?page=${pageId}`
+    );
     return data;
   } catch (error) {
-    // throw new Error("Unable to fetch data");
-    console.log(error);
+    throw new Error("Unable to fetch data");
   }
 };
 
 const Home = () => {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["users"],
-    queryFn: fetchData,
-  });
+  const { id } = useParams();
+  const pageId = parseInt(id);
+  let navigate = useNavigate();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  const toast = useToast();
+  const { data, isLoading } = useQuery(
+    ["posts", pageId],
+    () => fetchData(pageId),
+    {
+      keepPreviousData: true,
+      onError: (error) => {
+        toast({ status: "error", title: error.message });
+      },
+    }
+  );
 
   return (
-    <div>
-      <Heading>Welcome TO React Query</Heading>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
+    <Container mt={"4"} maxW={"1300px"}>
+      <Text>Current Page: {pageId}</Text>
+      {isLoading ? (
+        <Grid placeItems={"center"} height={"100vh"}>
+          <Spinner />
+        </Grid>
+      ) : (
+        <>
+          {data &&
+            data.data &&
+            data.data.map((post) => (
+              <Stack
+                p="4"
+                mt={"4"}
+                boxShadow={"md"}
+                borderRadius={"xl"}
+                border={"1px solid #ccc"}
+                key={post.id}
+              >
+                <Flex justify={"space-between"}>
+                  <Text>userId: {post.user_id}</Text>
+                  <Text>postId:{post.id}</Text>
+                </Flex>
+                <Heading>{post.title}</Heading>
+                <Text>{post.body}</Text>
+              </Stack>
+            ))}
+          <Flex justify={"space-evenly"} m={"4"}>
+            <Button
+              colorScheme="yellow"
+              onClick={() => {
+                if (pageId > 1) {
+                  navigate(`/${pageId - 1}`);
+                }
+              }}
+              disabled={pageId <= 1}
+            >
+              Prev
+            </Button>
+
+            <Button
+              colorScheme="pink"
+              onClick={() => {
+                navigate(`/${pageId + 1}`);
+              }}
+            >
+              Next
+            </Button>
+          </Flex>
+        </>
+      )}
+    </Container>
   );
 };
 
-export default Home; // Export Home component
+export default Home;
